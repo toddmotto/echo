3b323bc66eab4f07a2c9bfa6575f1de359841b7c
Todd Motto <toddmotto@todds-mbp.default>
2013-11-16 16:39:12 +0000

    Save 2 bytes

--- dist/echo.js
+++ dist/echo.js
@@ -13,7 +13,7 @@ window.Echo = (function (window, document, undefined) {
 
   var _inView = function (img) {
     var coords = img.getBoundingClientRect();
-    return ((coords.top >= 0 && coords.left >= 0 && coords.top) <= (window.innerHeight || document.documentElement.clientHeight));
+    return (coords.top >= 0 && coords.left >= 0 && coords.top) <= (window.innerHeight || document.documentElement.clientHeight);
   };
 
   var _pollImages = function () {
