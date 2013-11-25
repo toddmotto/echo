df3f6eb47efa9949907404eb785b09a8b76c8089
Morozov Andrew <anmorozov23@yandex.ru>
2013-11-26 00:21:39 +0200

    querySelectorAll for ie8-

--- src/echo.js
+++ src/echo.js
@@ -1,3 +1,16 @@
+if (!document.querySelectorAll && document.createStyleSheet) {
+  document.querySelectorAll = function (selector) {
+    var all = document.all, result = [], style = document.createStyleSheet();
+    style.addRule(selector, "fake:fake");
+    for (var i = all.length - 1; i >= 0; i--) {
+      if (all[i].currentStyle.fake === 'fake') {
+        result.push(all[i]);
+      }
+    }
+    style.removeRule(0); return result;
+  }
+}
+
 window.Echo = (function (window, document, undefined) {
 
   'use strict';
